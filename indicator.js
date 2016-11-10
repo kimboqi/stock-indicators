60366131cf78f8d43b34c0bf6228207b6e2f05c9
kq <qijianbiao2009@gmail.com>
2016-11-10 13:23:51 +0800

    finish macd and obv

--- indicator.js
+++ indicator.js
@@ -4,7 +4,7 @@ var Indicator = (function(){
    *
    * @method obv
    * @param {Array[Array[Number, Number]]} ticks
-   * ticks为二维数组类型，其中内层数组第一个值为收盘价格，第二个值为成交量
+   * ticks为二维数组类型，其中内层数组第一个值为收盘价，第二个值为成交量
    * @return {Array[number]} obvs
    */
   var obv = function (ticks) {
@@ -12,7 +12,7 @@ var Indicator = (function(){
     for (var i = 0; i < length; i++) {
       var value = 0, curTick = ticks[i];
       if (i != 0) {
-        var lastObvValue = ticks[i-1];
+        var lastObvValue = obvs[i-1];
         if (curTick[0] >= lastTick[0]) {
           value = lastObvValue + curTick[1];
         } else {
@@ -51,14 +51,14 @@ var Indicator = (function(){
         ema26.push(c);
         deas.push(0);
       } else {
-        ema12.push(ema(emas12[i-1], c, 12));
-        ema26.push(ema(emas26[i-1], c, 26));
+        ema12.push(ema(ema12[i-1], c, 12));
+        ema26.push(ema(ema26[i-1], c, 26));
       }
       diffs.push(ema12[i] - ema26[i]);
       if (i != 0) {
         deas.push(dea(deas[i-1], diffs[i]));
       }
-      bars.push(diffs[i]-deas[i]);
+      bars.push((diffs[i]-deas[i]) * 2);
     }
     return {diffs: diffs, deas: deas, bars: bars};
   };
--- indicator.test.js
+++ indicator.test.js
@@ -1,9 +1,9 @@
 var Indicator = require('./indicator.js');
 
 describe('obv test', function(){
-  console.log(Indicator.OBV([[0, 1], [2, 3]]));
+  console.log(Indicator.OBV([[11.94, 9300], [13.13, 3600], [14.44, 2000]]));
 });
 
 describe('macd test', function(){
-  console.log(Indicator.OBV([1, 3]));
+  console.log(Indicator.MACD([11.94, 13.13, 14.44]));
 });
