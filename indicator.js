078642aadd64f22f905dca35d2dbb9f5ab608162
kq <qijianbiao2009@gmail.com>
2016-11-14 15:19:46 +0800

    add boll doc

--- indicator.js
+++ indicator.js
@@ -121,6 +121,16 @@ var Indicator = (function(){
     return {"k": ks, "d": ds, "j": js};
   };
 
+  /**
+   *
+   * 计算boll指标,ma的周期为20日
+   *
+   * @method boll
+   * @param {Array[Number]} ticks
+   * 一维数组类型，每个元素为当前Tick的收盘价格
+   * @return {Object} 返回一个包含upper mid lower属性的对象,每个属性对应的类型为{Array[Number]}
+   */
+
   var boll = function(ticks) {
     //移动平均线周期为20
     var maDays = 20, tickBegin = maDays - 1, maSum = 0, p = 0;
