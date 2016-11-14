6d1b64704a73c4d99372564e4cf0543385a7c9fa
kq <qijianbiao2009@gmail.com>
2016-11-14 13:45:47 +0800

    finish kdj

--- indicator.js
+++ indicator.js
@@ -62,9 +62,68 @@ var Indicator = (function(){
     }
     return {diffs: diffs, deas: deas, bars: bars};
   };
+
+  var getMaxHighAndMinLow = function (ticks) {
+    var maxHigh = ticks[0][0], minLow = ticks[0][1];
+    for (var i = 0; i < ticks.length; i++) {
+      var t = ticks[i], high = t[0], low = t[1];
+      if (high > maxHigh) {
+        maxHigh = high;
+      }
+      if (low < minLow) {
+        minLow = low;
+      }
+    }
+    return [maxHigh, minLow];
+  };
+
+  /**
+   *
+   * 计算kdj指标,rsv的周期为9日
+   *
+   * @method kdj
+   * @param {Array[Array[Number, Number, Number]]} ticks
+   * 二维数组类型，其中内层数组包含三个元素值，第一个值表示当前Tick的最高价格，第二个表示当前Tick的最低价格，第三个表示当前Tick的收盘价格
+   * @return {Object} 返回一个包含k d j属性的对象,每个属性对应的类型为{Array[Number]}
+   */
+  var kdj = function (ticks) {
+    var nineDaysTicks = [], days = 9, rsvs = [];
+    var ks = [], ds = [], js = [];
+    var lastK, lastD, curK, curD;
+    var maxAndMin, max, min;
+    for (var i = 0; i < ticks.length; i++) {
+      var t = ticks[i], close = t[2];
+      nineDaysTicks.push(t);
+      maxAndMin = getMaxHighAndMinLow(nineDaysTicks);
+      max = maxAndMin[0];
+      min = maxAndMin[1];
+      if (max == min) {
+        rsvs.push(0);
+      } else {
+        rsvs.push((close - min) / (max - min) * 100);
+      }
+      if (nineDaysTicks.length == days) {
+        nineDaysTicks.shift();
+      }
+      if (i == 0) {
+        lastK = lastD = rsvs[i];
+      }
+      curK = 2 / 3 * lastK + 1 / 3 * rsvs[i];
+      ks.push(curK);
+      lastK = curK;
+
+      curD = 2 / 3 * lastD + 1 / 3 * curK;
+      ds.push(curD);
+      lastD = curD;
+
+      js.push(3 * curK - 2 * curD);
+    }
+    return {"k": ks, "d": ds, "j": js};
+  };
   return {
     "OBV": obv,
-    "MACD": macd
+    "MACD": macd,
+    "KDJ": kdj
   };
 })();
 
--- indicator.test.js
+++ indicator.test.js
@@ -1,9 +1,15 @@
 var Indicator = require('./indicator.js');
-
+//江苏银行
 describe('obv test', function(){
   console.log(Indicator.OBV([[11.94, 9300], [13.13, 3600], [14.44, 2000]]));
 });
 
+var testArray = [9.03, 9.93, 10.92, 12.01, 11.46, 12.18];
 describe('macd test', function(){
-  console.log(Indicator.MACD([11.94, 13.13, 14.44]));
+  console.log(Indicator.MACD(testArray));
+});
+
+testArray = [[9.03, 7.52, 9.03], [9.93, 9.93, 9.93], [10.92, 10.92, 10.92], [12.01, 11.20, 12.01], [11.80, 11.28, 11.46], [12.49, 11.38, 12.19]];
+describe('kdj test', function(){
+  console.log(Indicator.KDJ(testArray));
 });
